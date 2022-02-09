0be1225ae6c741104d65feb61e324642c25bd63b
nabil <merchant.nabil@gmail.com>
2022-02-09 10:37:06 -0600

    Adding a delete button and functionality.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ['tag1', 'tag2', 'tag3'],
+    value: this.props.counter.value,
   };
 
   // constructor() {
@@ -14,7 +13,7 @@ class Counter extends Component {
   handleIncrement = (product) => {
     console.log(product);
     // Updating the counter
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   doHandleIncrement = () => {
@@ -31,18 +30,24 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
+    classes += this.state.value === 0 ? 'warning' : 'primary';
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? 'Zero' : count;
   }
 }
