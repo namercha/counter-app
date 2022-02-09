e4d18577c7e030806588bfb4703f798746c5bf27
nabil <merchant.nabil@gmail.com>
2022-02-09 10:51:56 -0600

    Adding incrementing functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
-
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
-
-  handleIncrement = (product) => {
-    console.log(product);
-    // Updating the counter
-    this.setState({ value: this.state.value + 1 });
-  };
 
   doHandleIncrement = () => {
     this.handleIncrement({ id: 1 });
@@ -25,7 +15,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({})}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -42,12 +32,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
-    classes += this.state.value === 0 ? 'warning' : 'primary';
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? 'Zero' : count;
   }
 }
